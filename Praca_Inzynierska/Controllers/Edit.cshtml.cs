9799ccc46dad655b08aa40bcc1e9b3d9b83be0be
Adam Wroński <adam.wronski96@gmail.com>
2019-12-15 00:03:55 +0100

    s

---
+++ Praca_Inzynierska/Controllers/Edit.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Praca_Inzynierska.Models;
+using Praca_Inzynierska.Persistence;
+
+namespace Praca_Inzynierska
+{
+    public class EditModel : PageModel
+    {
+        private readonly Praca_Inzynierska.Persistence.AppDbContext _context;
+
+        public EditModel(Praca_Inzynierska.Persistence.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Actor Actor { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Actor = await _context.Actors.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Actor == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Actor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ActorExists(Actor.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ActorExists(int id)
+        {
+            return _context.Actors.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Praca_Inzynierska/Controllers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Praca_Inzynierska.Models;
+using Praca_Inzynierska.Persistence;
+
+namespace Praca_Inzynierska
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Praca_Inzynierska.Persistence.AppDbContext _context;
+
+        public IndexModel(Praca_Inzynierska.Persistence.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Actor> Actor { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Actor = await _context.Actors.ToListAsync();
+        }
+    }
+}
